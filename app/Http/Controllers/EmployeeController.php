a71b0afe0286c8509f1ae22b8e6b0e8b41a81770
asquiambao <aquiambao061095@gmail.com>
2024-07-28 21:18:32 +0800

    Add Yajra Datatables in Laravel

---
+++ app/Http/Controllers/EmployeeController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Employee;
+use Illuminate\Http\Request;
+use Yajra\DataTables\DataTables;
+
+class EmployeeController extends Controller
+{
+    public function index(Request $request)
+    {
+        if ($request->ajax()) {
+            $data = Employee::all();
+            return DataTables::of($data)
+                ->addIndexColumn()
+                ->addColumn('action', function ($row) {
+                    $btn = '<a href="javascript:void(0)" class="btn btn-primary btn-sm">View</a>';
+                    return $btn;
+                })
+                ->rawColumns(['action'])
+                ->make(true);
+        }
+        return view('employee');
+    }
+}
