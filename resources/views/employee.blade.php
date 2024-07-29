1cdd198001fc27741d55e4285dcca3319715e083
asquiambao <aquiambao061095@gmail.com>
2024-07-29 10:03:46 +0800

    Implementing datatables buttons

--- resources/views/employee.blade.php
+++ resources/views/employee.blade.php
@@ -7,8 +7,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.datatables.net/buttons/1.0.3/css/buttons.dataTables.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link href="https://cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css" rel="stylesheet">
     <link href="https://cdn.datatables.net/1.10.19/css/dataTables.bootstrap4.min.css" rel="stylesheet">
     @vite(['resources/css/app.css','resources/js/app.js'])
@@ -22,8 +22,8 @@
                 <th> # </th>
                 <th> Name </th>
                 <th> Gender </th>
-                <th> Email </th>
                 <th> Phone </th>
+                <th> Address </th>
                 <th> Action </th>
             </thead>
             <tbody>
@@ -31,16 +31,14 @@
         </table>
     </div>
 
-    <script src="https://code.jquery.com/jquery-3.5.0.js" integrity="sha256-r/AaFHrszJtwpe+tHyNi/XCfMxYpbsRg2Uqn0x3s2zc="
-        crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
-        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.5.0.js" integrity="sha256-r/AaFHrszJtwpe+tHyNi/XCfMxYpbsRg2Uqn0x3s2zc=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
     </script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
-        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
     </script>
     <script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
     <script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/1.6.4/js/dataTables.buttons.min.js"></script>
 
     <script type="text/javascript">
         $(document).ready(function() {
@@ -62,12 +60,12 @@
                         name: 'gender'
                     },
                     {
-                        data: 'email',
-                        name: 'email'
-                    },
-                    {
                         data: 'phone_number',
                         name: 'phone_number'
+                    },
+                    {
+                        data: 'address',
+                        name: 'address'
                     },
                     {
                         data: 'action',
@@ -75,6 +73,13 @@
                         orderable: false,
                         searchable: false,
                     }
+                ],
+                language: {
+                    processing: '<i class="fas fa-spinner fa-spin fa-3x fa-fw"></i>'
+                },
+                dom: 'Blfrtip',
+                buttons: [
+                    'copy', 'excel',
                 ],
                 columnDefs: [{
                         // User full name
@@ -92,21 +97,21 @@
                                 $initials = $name.match(/\b\w/g) || [],
                                 $output;
                             $initials = (($initials.shift() || '') + ($initials.pop() || '')).toUpperCase();
-                            $output = '<span class="font-medium text-gray-600 dark:text-gray-300">'+ $initials +'</span>';
+                            $output = '<span class="font-medium text-gray-600 dark:text-gray-300">' + $initials + '</span>';
 
-                            var $row_output = '<div class="flex items-center gap-4">' + 
-                            '<div class="relative inline-flex items-center justify-center w-10 h-10 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">' +  
-                            '<span class="font-medium text-gray-600 dark:text-gray-300">'+ $initials  + '</span>' +
-                            '</div>' + 
-                            '<div class="font-medium dark:text-white">' + 
-                            '<div>' +
-                                    '<a href="' +
+                            var $row_output = '<div class="flex items-center gap-4">' +
+                                '<div class="relative inline-flex items-center justify-center w-10 h-10 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">' +
+                                '<span class="font-medium text-gray-600 dark:text-gray-300">' + $initials + '</span>' +
+                                '</div>' +
+                                '<div class="font-medium dark:text-white">' +
+                                '<div>' +
+                                '<a href="' +
                                 userView +
                                 '" class="text-body text-truncate"><span class="fw-medium">' +
                                 $name +
                                 '</span></a>' + '</div>' +
-                            '<div class="text-sm text-gray-500 dark:text-gray-400">'+ $email + '</div>' + 
-                            '</div>' + '</div>';
+                                '<div class="text-sm text-gray-500 dark:text-gray-400">' + $email + '</div>' +
+                                '</div>' + '</div>';
 
                             return $row_output;
                         }
@@ -118,14 +123,15 @@
                         searchable: false,
                         orderable: false,
                         render: function(data, type, full, meta) {
-                            return('<div class="flex space-x-2 mt-2">' + 
-                            '<button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-3 py-2.5 text-center mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"><i class="fas fa-pencil me-1"></i>Edit</button>'+ 
-                             '<button type="button" class="text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-sm px-3 py-2.5 text-center mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"><i class="fas fa-trash me-1"></i>Delete</button></div>');
+                            return ('<div class="flex space-x-2 mt-2">' +
+                                '<button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-3 py-2.5 text-center mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"><i class="fas fa-pencil me-1"></i>Edit</button>' +
+                                '<button type="button" class="text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-sm px-3 py-2.5 text-center mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"><i class="fas fa-trash me-1"></i>Delete</button></div>');
                         }
                     }
                 ],
+        
+                });
             });
-        });
     </script>
 </body>
 
